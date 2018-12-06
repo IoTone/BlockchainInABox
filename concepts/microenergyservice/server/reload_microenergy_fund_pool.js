268564aa578e8c571def0b9a761cc9714be1d63d
David J. Kordsmeier <dkords@gmail.com>
2018-12-05 23:34:22 -0800

    refs #2 Get a send mosaic transaction working
    Part of the problem is the docs are a mess for this stuff.  Nem has 3 or
    more sdks.  There is a messy trail to follow to figure out what to use.
    I'm using a mix of the sdk and the old nem-library.

--- concepts/microenergyservice/server/reload_microenergy_fund_pool.js
+++ concepts/microenergyservice/server/reload_microenergy_fund_pool.js
@@ -1,4 +1,4 @@
-var nemlib = require('nem-library');
+// var nemlib = require('nem-library');
 
 var NEMLibrary = require('nem-library').NEMLibrary;
 var NetworkTypes = require('nem-library').NetworkTypes;
@@ -8,18 +8,30 @@ var BrainPassword = require("nem-library").BrainPassword;
 var argv = require('yargs').argv
 var fs = require('fs');
 var nemSdk = require("nem-sdk").default;
+var walletPriv = require('./wallet.json');
 var walletLoad = fs.readFileSync("./wallet.wlt");
+var nemnet;
 
 if (CONFIG.nem_net === "mainnet") {
     NEMLibrary.bootstrap(NetworkTypes.MAIN_NET);
+    nemnet = nemSdk.model.network.data.mainnet;
 } else if (CONFIG.nem_net === "testnet") {
     NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
+    nemnet = nemSdk.model.network.data.testnet;
 } else if (CONFIG.nem_net === "mijinnet") {
     NEMLibrary.bootstrap(NetworkTypes.MIJIN_NET);
+    nemnet = nemSdk.model.network.data.mijin;
 } else {
     // Default to main
     NEMLibrary.bootstrap(NetworkTypes.MAIN_NET);
+    nemnet = nemSdk.model.network.data.mainnet;
 }
+
+console.log("nemnet:", nemnet);
+
+// Create an NIS endpoint object
+var endpoint = nemSdk.model.objects.create("endpoint")(nemSdk.model.nodes.defaultMainnet, nemSdk.model.nodes.defaultPort);
+
 
 var decodedArray = walletLoad.toString('utf8');
 
@@ -33,26 +45,45 @@ try {
 }
 
 // unlock privatekey
+var common = walletPriv; // wallet.unlockPrivateKey(walletPriv.brainpassword);
 
-/*
+//
+// Create variable to store our mosaic definitions, needed to calculate fees properly (already contains xem definition)
+var mosaicDefinitionMetaDataPair = nemSdk.model.objects.get("mosaicDefinitionMetaDataPair");
 
-var decodedArray = walletLoad.toString('utf8');
-var decodedWordArray = nemSdk.crypto.js.enc.Utf8.parse(JSON.stringify(newbrainWallet));
+// Create an un-prepared mosaic transfer transaction object (use same object as transfer tansaction)
+var transferTransaction = nemSdk.model.objects.create("transferTransaction")(CONFIG.smarthome_config.nem_microenergy_owner_address, 1, "Hello");
 
-// Create a common object holding key
-// var common = nem.model.objects.create("common")("", "Private key");
+// Create the mosaic attachment
+var mosaicAttachment = nemSdk.model.objects.create("mosaicAttachment")(CONFIG.smarthome_config.nem_mciroenergy_mosaic_namespace, CONFIG.smarthome_config.nem_microenergy_mosaic_name, 1000); // 100 nw.fiat.eur (divisibility is 2 for this mosaic)
 
-// Create a mosaic transfer from Address A to Address B
+// Push attachment into transaction mosaics
+transferTransaction.mosaics.push(mosaicAttachment);
 
-// Create a common object
-var common = nem.model.objects.create("common")("walletPassword/passphrase", "");
+// Need mosaic definition of nw.fiat:eur to calculate adequate fees, so we get it from network.
+// Otherwise you can simply take the mosaic definition from api manually (http://bob.nem.ninja/docs/#retrieving-mosaic-definitions) 
+// and put it into mosaicDefinitionMetaDataPair model (objects.js) next to nem:xem (be careful to respect object structure)
+nemSdk.com.requests.namespace.mosaicDefinitions(endpoint, mosaicAttachment.mosaicId.namespaceId).then(function(res) {
+	// Look for the mosaic definition(s) we want in the request response (Could use ["eur", "usd"] to return eur and usd mosaicDefinitionMetaDataPairs)
+	var neededDefinition = nemSdk.utils.helpers.searchMosaicDefinitionArray(res.data, [CONFIG.smarthome_config.nem_microenergy_mosaic_name]);
+	
+	// Get full name of mosaic to use as object key
+	var fullMosaicName  = nemSdk.utils.format.mosaicIdToName(mosaicAttachment.mosaicId);
 
-// Get the wallet account to decrypt
-var walletAccount = wallet.accounts[index];
+	// Check if the mosaic was found
+	if(undefined === neededDefinition[fullMosaicName]) return console.error("Mosaic not found !");
 
-// Decrypt account private key 
-nem.crypto.helpers.passwordToPrivatekey(common, walletAccount, wallet.algo);
+	// Set eur mosaic definition into mosaicDefinitionMetaDataPair
+	mosaicDefinitionMetaDataPair[fullMosaicName] = {};
+	mosaicDefinitionMetaDataPair[fullMosaicName].mosaicDefinition = neededDefinition[fullMosaicName];
 
-// The common object now has a private key
-console.log(common)
-*/
+	// Prepare the transfer transaction object
+	var transactionEntity = nemSdk.model.transactions.prepare("mosaicTransferTransaction")(common, transferTransaction, mosaicDefinitionMetaDataPair, nemnet.id);
+
+	// Serialize transfer transaction and announce
+    nemSdk.model.transactions.send(common, transactionEntity, endpoint);
+    console.log("sent transaction");
+}, 
+function(err) {
+	console.error(err);
+});
