e731af6dbd9c8bbdf82466068465619e43277a95
David J. Kordsmeier <dkords@gmail.com>
2018-12-18 21:57:53 -0800

    refs #2 More net config cleanup

--- concepts/microenergyservice/server/reload_microenergy_fund_pool.js
+++ concepts/microenergyservice/server/reload_microenergy_fund_pool.js
@@ -10,27 +10,29 @@ var fs = require('fs');
 var nemSdk = require("nem-sdk").default;
 var walletPriv = require('./wallet.json');
 var walletLoad = fs.readFileSync("./wallet.wlt");
+
 var nemnet;
+var nemnode;
 
 if (CONFIG["nem_net_config"].net === "mainnet") {
-    NEMLibrary.bootstrap(NetworkTypes.MAIN_NET);
     nemnet = nemSdk.model.network.data.mainnet;
+    nemnode = nemSdk.model.nodes.defaultMainnet;
 } else if (CONFIG["nem_net_config"].net === "testnet") {
-    NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
     nemnet = nemSdk.model.network.data.testnet;
+    nemnode = nemSdk.model.nodes.defaultTestnet;
 } else if (CONFIG["nem_net_config"].net === "mijinnet") {
-    NEMLibrary.bootstrap(NetworkTypes.MIJIN_NET);
     nemnet = nemSdk.model.network.data.mijin;
+    nemnode = nemSdk.model.nodes.defaultMijin;
 } else {
     // Default to main
-    NEMLibrary.bootstrap(NetworkTypes.MAIN_NET);
     nemnet = nemSdk.model.network.data.mainnet;
+    nemnode = nemSdk.model.nodes.defaultMainnet;
 }
 
 console.log("nemnet:", nemnet);
 
 // Create an NIS endpoint object
-var endpoint = nemSdk.model.objects.create("endpoint")(nemSdk.model.nodes.defaultMainnet, nemSdk.model.nodes.defaultPort);
+var endpoint = nemSdk.model.objects.create("endpoint")(nemnode, nemSdk.model.nodes.defaultPort);
 
 
 var decodedArray = walletLoad.toString('utf8');
